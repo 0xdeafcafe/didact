5e7042a4f860bbb6152b10ccc1bf2a3e572dd45d
Alex Forbes-Reed <alexreed@outlook.com>
2016-06-29 17:06:20 +0100

    undo excessive logic

--- src/Didact/DidactClientExtensions.cs
+++ src/Didact/DidactClientExtensions.cs
@@ -1,12 +1,9 @@
-using Didact.Models;
-
 namespace Didact
 {
 	public static class DidactClientExtensions
 	{
 		public static DidactClient Version(this DidactClient didact, string version)
 		{
-			didact.Metadata = didact.Metadata ?? new Metadata();
 			didact.Metadata.Version = version;
 			return didact;
 		}
