389c58b64e2cd49e57120214d745204ad30c2906
Alex Forbes-Reed <alexreed@outlook.com>
2016-07-07 19:32:25 +0100

    added metadata partial for extensions

--- src/Didact/DidactClientExtensions.cs
+++ src/Didact/DidactClientExtensions.cs
@@ -7,30 +7,6 @@ namespace Didact
 {
 	public static partial class DidactClientExtensions
 	{
-		public static DidactClient Name(this DidactClient didact, string name)
-		{
-			didact.Metadata.Name = name;
-			return didact;
-		}
-		
-		public static DidactClient CliName(this DidactClient didact, string cliName)
-		{
-			didact.Metadata.CliName = cliName;
-			return didact;
-		}
-
-		public static DidactClient Version(this DidactClient didact, string version)
-		{
-			didact.Metadata.Version = version;
-			return didact;
-		}
-
-		public static DidactClient Usage(this DidactClient didact, string usage)
-		{
-			didact.Metadata.Usage = usage;
-			return didact;
-		}
-
 		internal static void PrintHelp(DidactClient client)
 		{
 			if (client.Metadata.Usage == null)
