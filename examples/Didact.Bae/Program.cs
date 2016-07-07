f6b891f9f481e3e0b6a5809c0298f3b53f698c8e
Alex Forbes-Reed <alexreed@outlook.com>
2016-07-07 15:10:48 +0100

    Added one more command to the Taylor Swift example

--- examples/Didact.Bae/Program.cs
+++ examples/Didact.Bae/Program.cs
@@ -15,6 +15,7 @@ namespace ConsoleApplication
 				.Version("1.0.0")
 				.Usage("get-song <song> [options]")
 				.Option("-k, --api-key [api-key]", "The Api Key for your account on baelor.io.")
+				.Command("get-album <album>", "Get's details of a Taylor Swift album.", GetAlbum)
 				.Command("get-lyrics <song>", "Gets the lyric of a Taylor Swift song.", GetLyricsToSong)
 					.Option("-s, --show-timecodes [show-timecodes]", "Toggles the visibility of Timecodes", defaultValue: "t");
 
@@ -35,6 +36,24 @@ namespace ConsoleApplication
 				if (showTimecodes == "t") Write($"{lyric.TimeCode} - ");
 				Write($"{lyric.Content}\n");
 			}
+		}
+
+		public static async Task GetAlbum(Dictionary<string, string> arguments, Dictionary<string, string> options)
+		{
+			var albumSlug = arguments["album"];
+			var apiKey = options["api-key"];
+
+			var client = new BaelorClient(apiKey);
+			var album = await client.Album(albumSlug);
+			WriteLine(album.Name);
+			WriteLine($"- Produced By: {string.Join(", ", album.Producers)}");
+			WriteLine($"- Genres: {string.Join(", ", album.Genres)}");
+			WriteLine($"- Released: {album.ReleasedAt.ToString("dd MMM yyyy")}");
+			WriteLine($"- Label: {album.Label}");
+			WriteLine($"- Length: {album.Length.ToString("c")}");
+			WriteLine($"- Songs:");
+			foreach (var song in album.Songs)
+				WriteLine($"  - {song.Title}");
 		}
 	}
 }
