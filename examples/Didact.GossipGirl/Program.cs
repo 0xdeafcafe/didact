40bfaaa6cb975340cbb10bfdd76fb6b781f434cb
Alex Forbes-Reed <alexreed@outlook.com>
2016-07-07 21:03:44 +0100

    Created a simpler Gossip Girl example

---
+++ examples/Didact.GossipGirl/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Didact;
+
+namespace Didact.GossipGirl
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var didact = new DidactClient()
+				.CliName("gossip-girl")
+				.Name("Gossip Girl Cli Tool")
+				.Version("1.0.0")
+				.Command("add-gossip <word>", "Adds gossip to a word.", (arguments, options) =>
+				{
+					const string gossip = "xo";
+					var word = arguments["word"];
+					var gossips = int.Parse(options["c"]);
+
+					for(var i = 0; i < gossips; i++)
+						word += gossip;
+
+					Console.WriteLine(word);
+				})
+					.Option("-c, --count [count]", "Number of gossips to add.", validate: (val) => 
+					{
+						int gossips = 0;
+						return Int32.TryParse(val, out gossips);
+					}, defaultValue: "2");
+
+			didact.Parse(args);
+		}
+	}
+}
