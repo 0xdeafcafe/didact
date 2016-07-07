1ec13ed0a2eca5c791b82be257228bf926a3d6ac
Alex Forbes-Reed <alexreed@outlook.com>
2016-07-07 14:32:04 +0100

    Example BaelorSwift cli tool

--- examples/Didact.Bae/Program.cs
+++ examples/Didact.Bae/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BaelorNet;
+using Didact;
+using static System.Console;
 
 namespace ConsoleApplication
 {
@@ -6,7 +10,31 @@ namespace ConsoleApplication
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var didact = new DidactClient()
+				.Name("Baelor Cli Client")
+				.Version("1.0.0")
+				.Usage("get-song <song> [options]")
+				.Option("-k, --api-key [api-key]", "The Api Key for your account on baelor.io.")
+				.Command("get-lyrics <song>", "Gets the lyric of a Taylor Swift song.", GetLyricsToSong)
+					.Option("-s, --show-timecodes [show-timecodes]", "Toggles the visibility of Timecodes", defaultValue: "t");
+
+			didact.ParseAsync(args).Wait();
+		}
+
+		public static async Task GetLyricsToSong(Dictionary<string, string> arguments, Dictionary<string, string> options)
+		{
+			var songSlug = arguments["song"];
+			var showTimecodes = options["show-timecodes"];
+			var apiKey = options["api-key"];
+
+			var client = new BaelorClient(apiKey);
+			var song = await client.Song(songSlug);
+			WriteLine($"Lyrics for {song.Title}:");
+			foreach(var lyric in song.Lyrics)
+			{
+				if (showTimecodes == "t") Write($"{lyric.TimeCode} - ");
+				Write($"{lyric.Content}\n");
+			}
 		}
 	}
 }
